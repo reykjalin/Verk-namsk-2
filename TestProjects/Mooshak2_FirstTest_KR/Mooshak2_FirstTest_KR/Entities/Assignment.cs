d5e10ff60a0c6959270fe8cb8327370a7d4caa72
reykjalin <kristoferrt@gmail.com>
2016-05-01 18:02:04 +0000

    added db connection

---
+++ TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Entities/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2_FirstTest_KR.Entities
+{
+    public class Assignment
+    {
+        public uint assignmentId { get; set; }
+        public uint courseId { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public uint weight { get; set; }
+    }
+}
