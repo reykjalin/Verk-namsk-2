e2755d99317510e52bde4b01da9f4aa8d3213e23
reykjalin <kristoferrt@gmail.com>
2016-05-02 18:54:31 +0000

    Added AssignmentController and CourseController classes

--- TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Controllers/AssignmentController.cs
+++ TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Controllers/AssignmentController.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Mooshak2_FirstTest_KR.Services;
+using System.Web.Mvc;
 
 namespace Mooshak2_FirstTest_KR.Controllers
 {
-    public class AssignmentController
+    public class AssignmentController : Controller
     {
+        private AssignmentService service;
+
+        public AssignmentController() { service = new AssignmentService(); }
+
+        public ActionResult create() { return View(); }
+
+        public ActionResult edit() { return View(); }
+
+        public ActionResult remove() { return View(); }
+
+        public ActionResult details(int id) { return View(); }
+
+        public ActionResult assignmentList() { return View(); }
     }
 }
--- TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Controllers/CourseController.cs
+++ TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Controllers/CourseController.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Mooshak2_FirstTest_KR.Services;
 
 namespace Mooshak2_FirstTest_KR.Controllers
 {
-    public class CourseController
+    public class CourseController : Controller
     {
+        private CourseService service;
+
+        public CourseController() { service = new CourseService(); }
+
+        public ActionResult create() { return View(); }
+
+        public ActionResult edit() { return View(); }
+
+        public ActionResult remove() { return View(); }
+
+        public ActionResult details(int id) { return View(); }
+
+        public ActionResult courseList() { return View(); }
     }
 }
