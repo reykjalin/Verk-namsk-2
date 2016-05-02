943487b400628b8d270d8b8f0d23c433da18a2dd
reykjalin <kristoferrt@gmail.com>
2016-05-02 18:45:55 +0000

    Assignment and Course service classes

---
+++ TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Models/ViewModels/AssignmentPartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2_FirstTest_KR.Models.ViewModels
+{
+    public class AssignmentPartViewModel
+    {
+    }
+}
---
+++ TestProjects/Mooshak2_FirstTest_KR/Mooshak2_FirstTest_KR/Services/CourseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshak2_FirstTest_KR.Models;
+using Mooshak2_FirstTest_KR.Models.ViewModels;
+
+namespace Mooshak2_FirstTest_KR.Services
+{
+    public class CourseService
+    {
+        private ApplicationDbContext database;
+
+        public CourseService() { database = new ApplicationDbContext(); }
+
+        public List<CourseViewModel> getAllCourses() { return new List<CourseViewModel>(); }
+
+        public List<CourseViewModel> getCourseById(int courseId) { return new List<CourseViewModel>(); }
+    }
+}
