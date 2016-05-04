9cf416c6decd7ce0fb5b8a895c7956051de75bc2
Eyþór <eythortra15@ru.is>
2016-05-04 23:21:33 +0000

    græjaði klasa og dót fyrir assignment, vinnufært fyrir morgundaginn

---
+++ MooshakV2/MooshakV2/MooshakV2/Services/AssignmentService.cs
@@ -0,0 +1,38 @@
+﻿using MooshakV2.Models;
+using MooshakV2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakV2.Services
+{
+    public class AssignmentService
+    {
+        private ApplicationDbContext database;
+        public AssignmentService()
+        {
+            database = new ApplicationDbContext();
+        }
+
+        public List<AssignmentViewModel> getAllAssignments()
+        {
+            return new List<AssignmentViewModel>();
+        }
+
+        public List<AssignmentViewModel> getAllAssignmentsInCourse(int courseId)
+        {
+            return new List<AssignmentViewModel>();
+        }
+
+        public List<AssignmentViewModel> getAssignmentInCourse(int courseId, int assignmentId)
+        {
+            return new List<AssignmentViewModel>();
+        }
+
+        public bool handInAssignment(int assignmentId, int courseId, string data)
+        {
+            return true;
+        }
+    }
+}
