7d8350e4808a4cc403cda51d32d866c0d0c0a040
reykjalin <kristoferrt@gmail.com>
2016-05-04 21:38:04 +0000

    CourseController ready. Now to test.

--- MooshakV2/MooshakV2/MooshakV2/Controllers/CourseController.cs
+++ MooshakV2/MooshakV2/MooshakV2/Controllers/CourseController.cs
@@ -15,20 +15,20 @@ namespace MooshakV2.Controllers
         public CourseController() { service = new CourseService(); }
 
         /// <summary>
-        /// Returns the view of the course list.
+        /// Sýnir List view-inu
         /// </summary>
         /// <returns></returns>
         public ActionResult Index() { return RedirectToAction("List"); }
 
         /// <summary>
-        /// Shows view for creating a course
+        /// Sýnir Create view-ið fyrir Course
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         public ActionResult create() { return View(); }
 
         /// <summary>
-        /// Adds 'newCourse' to the database
+        /// Bætir Course 'newCourse' við gagnagrunn.
         /// </summary>
         /// <param name="newCourse"></param>
         /// <returns></returns>
@@ -50,15 +50,19 @@ namespace MooshakV2.Controllers
 
 
         /// <summary>
-        /// Edit course with ID 'id'. Shows the edit view
+        /// Breyta Course með ID 'id'. Sýnir Edit View-ið.
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         public ActionResult edit(int? id)
         {
+            // Athuga hvort id sé null
             if (id.HasValue)
             {
+                // Sækja Course með ID 'id'
                 var model = service.getCourseById(id);
+
+                // Ef Course er til, senda model á View, annars error
                 if (model != null)
                     return View(model);
             }
@@ -66,7 +70,7 @@ namespace MooshakV2.Controllers
         }
 
         /// <summary>
-        /// Edits course in DB with same ID as 'course' using data from 'course'
+        /// Breytir Course í gagnagrunni með sama ID og 'course' með uppfærðum gögnum úr 'course'
         /// </summary>
         /// <param name="course"></param>
         /// <returns></returns>
@@ -84,12 +88,22 @@ namespace MooshakV2.Controllers
             return View(course);
         }
 
+        /// <summary>
+        /// Síða sem biður um staðfestingu á því hvort eigi að eyða Course með ID 'id'.
+        /// Fer á error síðu ef Course er ekki til, eða 'id' er null.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult remove(int? id)
         {
+            // Athuga hvort id sé null
             if (id.HasValue)
             {
+                // Finna Course sem á að eyða
                 var toRemove = service.getCourseById(id);
+
+                // Ef Course er til birta staðfestingar view, annars error.
                 if (toRemove != null)
                     return View(toRemove);
             }
@@ -97,13 +111,13 @@ namespace MooshakV2.Controllers
         }
 
         /// <summary>
-        /// Removes course with ID 'id' from database.
+        /// Eyðir Course með ID 'toRemove.id' úr gagnagrunni.
         /// </summary>
         /// <returns></returns>
         [HttpPost]
         public ActionResult remove(CourseViewModel toRemove)
         {
-            // toRemove eytt úr DB
+            // toRemove eytt úr DB, ef eitthvað mistekst birtist error view
             if (service.removeCourse(toRemove.id))
                 return RedirectToAction("List");
 
@@ -111,15 +125,19 @@ namespace MooshakV2.Controllers
         }
 
         /// <summary>
-        /// Show detail page of course with ID 'id'
+        /// Sýnir nánari upplýsingar um Course með ID 'id'
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
         public ActionResult details(int? id)
         {
+            // Athuga hvort id sé null
             if (id.HasValue)
             {
+                // Course með ID 'id' fundinn
                 var model = service.getCourseById(id);
+
+                // Ef Course er til, birta Details view, annars sýna Error view.
                 if (model != null)
                     return View(model);
             }
@@ -127,15 +145,20 @@ namespace MooshakV2.Controllers
         }
 
         /// <summary>
-        /// Shows a page with a list of courses available in the database.
+        /// Sýnir lista af öllum Courses í gagnagrunni.
         /// </summary>
         /// <returns></returns>
         public ActionResult list()
         {
+            // Fá lista af öllum Courses og birta hann.
             var model = service.getAllCourses();
             return View(model);
         }
 
+        /// <summary>
+        /// Sýnir Error view.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult error() { return View(); }
     }
 }
