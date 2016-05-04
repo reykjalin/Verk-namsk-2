09d1cbb94e48bbdbe0ccfbf7b430f6ef0ec7b9ca
Pum8a <jonthor94@gmail.com>
2016-05-04 13:30:14 +0000

    ég setti up verkefni

---
+++ JT_TestProject/JT_TestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JT_TestProject.Startup))]
+namespace JT_TestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
