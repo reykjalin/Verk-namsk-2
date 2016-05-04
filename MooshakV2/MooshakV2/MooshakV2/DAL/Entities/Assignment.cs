441d648045a9f30066a0fdad3b7e1687608641ab
reykjalin <kristoferrt@gmail.com>
2016-05-04 21:12:05 +0000

    Added data context and entities to DAL folder

---
+++ MooshakV2/MooshakV2/MooshakV2/DAL/Entities/Assignment.cs
@@ -0,0 +1,34 @@
+namespace MooshakV2.DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Assignment")]
+    public partial class Assignment
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Assignment()
+        {
+            AssignmentParts = new HashSet<AssignmentPart>();
+        }
+
+        public int id { get; set; }
+
+        public int courseId { get; set; }
+
+        [Required]
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public int weight { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AssignmentPart> AssignmentParts { get; set; }
+    }
+}
