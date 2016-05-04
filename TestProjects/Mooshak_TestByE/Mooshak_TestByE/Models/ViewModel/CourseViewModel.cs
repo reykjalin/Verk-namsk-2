3fe886d1c6524585810e6dfccb27e44935722268
Eyþór <eythortra15@ru.is>
2016-05-04 15:36:48 +0000

    Test by E

---
+++ TestProjects/Mooshak_TestByE/Mooshak_TestByE/Models/ViewModel/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak_TestByE.Models.ViewModel
+{
+    public class CourseViewModel
+    {
+        string title;
+        string description;
+        int courseId;
+    }
+}
---
+++ TestProjects/Mooshak_TestByE/Mooshak_TestByE/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using Mooshak_TestByE.Models;
+using Mooshak_TestByE.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak_TestByE.Services
+{
+    public class CourseService
+    {
+        private ApplicationDbContext database;
+        public CourseService()
+        {
+            database = new ApplicationDbContext();
+        }
+
+        public List<CourseViewModel> getAllCourses()
+        {
+            return new List<CourseViewModel>();
+        }
+
+        public CourseViewModel getCourseById(int courseId)
+        {
+            return new CourseViewModel();
+        }
+    }
+}
