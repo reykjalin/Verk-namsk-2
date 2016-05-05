011856e108eb21edda4cc5236131c2af175a7050
reykjalin <kristoferrt@gmail.com>
2016-05-05 19:55:03 +0000

    Can edit users, including roles

--- MooshakV2/MooshakV2/MooshakV2/Controllers/UserController.cs
+++ MooshakV2/MooshakV2/MooshakV2/Controllers/UserController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using MooshakV2.Services;
+using MooshakV2.ViewModels;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace MooshakV2.Controllers
 {
@@ -18,20 +21,40 @@ namespace MooshakV2.Controllers
         public ActionResult create() { return RedirectToAction("Register", "Account"); }
 
         [HttpGet]
-        public ActionResult edit(int? id)
+        public ActionResult edit(string userName)
         {
-            if(id.HasValue)
+            if(!userName.IsEmpty())
             {
-                
+                var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var model = service.getUserByUserName(userName, userManager);
+                if(model != null)
+                    return View(model);
             }
-
             return View("Error");
+        }
+
+        [HttpPost]
+        public ActionResult edit(UserViewModel changedUser)
+        {
+            if(ModelState.IsValid)
+            {
+                var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                if(service.changeUser(changedUser, userManager))
+                    return RedirectToAction("List");
+            }
+            return View(changedUser);
         }
 
         public ActionResult list()
         {
-            var model = service.getAllUsers();
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var model = service.getAllUsers(userManager);
             return View(model);
+        }
+
+        private void prepareDropDown()
+        {
+            
         }
     }
 }
--- MooshakV2/MooshakV2/MooshakV2/Services/UserService.cs
+++ MooshakV2/MooshakV2/MooshakV2/Services/UserService.cs
@@ -8,6 +8,7 @@ using System.Web.Security;
 using MooshakV2.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
+using MooshakV2.Controllers;
 
 namespace MooshakV2.Services
 {
@@ -18,7 +19,7 @@ namespace MooshakV2.Services
 
         public UserService() { contextDb = new DatabaseDataContext(); }
 
-        public List<UserViewModel> getAllUsers()
+        public List<UserViewModel> getAllUsers(ApplicationUserManager userManager)
         {
             // Get all users
             var userList = (from user in contextDb.aspNetUsers
@@ -31,21 +32,80 @@ namespace MooshakV2.Services
                 UserViewModel userModel = new UserViewModel();
                 userModel.userName = user.UserName;
                 userModel.email = user.Email;
-                var role = roleManager.FindByName(user.UserName);
 
-                // Get role ID, default value is 4 (Student) if role isn't found.
                 try
                 {
-                    userModel.roleId = Convert.ToInt32(role.Id);
+                    var role = userManager.GetRoles(user.Id).FirstOrDefault();
+                    userModel.roleId = Convert.ToInt32((from roles in contextDb.aspNetRoles
+                                                        where roles.Name == role
+                                                        select roles.Id).FirstOrDefault());
                 }
-                catch(Exception e)
+                    catch (Exception e)
                 {
                     userModel.roleId = 4;
                 }
 
-                userListModel.Add(userModel);
+            userListModel.Add(userModel);
             }
             return userListModel;
+        }
+
+        public UserViewModel getUserByUserName(string userName, ApplicationUserManager userManager)
+        {
+            // 'userName' athugað í controller, veit að það er valid
+            var userEntity = (from user in contextDb.aspNetUsers
+                             where user.UserName == userName
+                             select user).SingleOrDefault();
+            if(userEntity != null)
+            {
+                UserViewModel user = new UserViewModel();
+                user.userName = userEntity.UserName;
+                user.email = userEntity.Email;
+                // Get role ID, default value is 4 (Student) if role isn't found.
+                try
+                {
+                    var role = userManager.GetRoles(userEntity.Id).FirstOrDefault();
+                    user.roleId = Convert.ToInt32((from roles in contextDb.aspNetRoles
+                                                   where roles.Name == role
+                                                   select roles.Id).FirstOrDefault());
+                }
+                catch(Exception e)
+                {
+                    user.roleId = 4;
+                }
+                return user;
+            }
+            return null;
+        }
+
+        public bool changeUser(UserViewModel newUserInfo, ApplicationUserManager userManager)
+        {
+            // TODO: Change to use userId. Add userId to UserViewModel
+            // Get usera information from userName
+            var user = (from users in contextDb.aspNetUsers
+                        where users.UserName == newUserInfo.userName
+                        select users).SingleOrDefault();
+            if(user != null)
+            {
+                user.UserName = newUserInfo.userName;
+                user.Email = newUserInfo.email;
+
+                // Get current user role
+                var role = userManager.GetRoles(user.Id).FirstOrDefault();
+                // If user didn't have a role, don't remove the user from his roles
+                if(role != null)
+                    userManager.RemoveFromRoles(user.Id, role);
+                // Get name of new role from DB
+                var roleName = (from roles in contextDb.aspNetRoles
+                                where roles.Id == newUserInfo.roleId.ToString()
+                                select roles.Name).FirstOrDefault();
+                // Add new role to user
+                userManager.AddToRole(user.Id, roleName);
+                // Update DB
+                contextDb.SaveChanges();
+                return true;
+            }
+            return false;
         }
     }
 }
