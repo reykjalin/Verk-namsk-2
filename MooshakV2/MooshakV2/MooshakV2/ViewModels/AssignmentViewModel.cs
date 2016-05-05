62f253652e9e6259cd5401fc89e7facc73395fdb
Eyþór <eythortra15@ru.is>
2016-05-05 12:16:45 +0000

    Create assignment komið

--- MooshakV2/MooshakV2/MooshakV2/ViewModels/AssignmentViewModel.cs
+++ MooshakV2/MooshakV2/MooshakV2/ViewModels/AssignmentViewModel.cs
@@ -8,10 +8,14 @@ namespace MooshakV2.ViewModels
 {
     public class AssignmentViewModel
     {
-        [Required(ErrorMessage="You must specify a name")]
-        private string title { get; set; }
-        private string description { get; set; }
-        private int weight { get; set; }
-        //private List<AssignmentPartViewModel> assignmentParts;
+        [Required(ErrorMessage="You must specify a name!")]
+        public string title { get; set; }
+        public string description { get; set; }
+        [Required(ErrorMessage="The assignment must have some weight")]
+        public int weight { get; set; }
+        public int id { get; set; }
+        [Required(ErrorMessage="You have to select course!")]
+        public int courseId { get; set; }
+        public List<AssignmentPartViewModel> assignmentParts { get; set; }
     }
 }
