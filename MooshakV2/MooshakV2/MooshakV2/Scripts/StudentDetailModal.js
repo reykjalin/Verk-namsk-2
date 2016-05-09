4d53a5d5939ebf53fb5fb37eba05b8c92fca2954
Sigurður Unnar <sigurduruh15@ru.is>
2016-05-09 14:01:47 +0000

    breytingar í studentDetail og list (student viwe)

---
+++ MooshakV2/MooshakV2/MooshakV2/Scripts/StudentDetailModal.js
@@ -0,0 +1,15 @@
+﻿$(".modalDetailButton").on('click', function () {
+    // Get user details
+    var title = $(this).attr('data-title');
+    var descr = $(this).attr('data-descr');
+    var weight = $(this).attr('data-weight');
+    var courseID = $(this).attr('data-courseID');
+
+
+    // Add text to modal dialog
+    $('#assignmentTitle').text(title);
+    $('#assignmentDescr').text(descr);
+    $('#assignmentWeight').text(weight);
+    $('#assignmentID').text(courseID);
+
+});
