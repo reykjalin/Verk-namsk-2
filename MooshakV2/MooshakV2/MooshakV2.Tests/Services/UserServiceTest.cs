660c44b6a9338798a3e84e2d8a3a4891971321d7
Sigurður Unnar <sigurduruh15@ru.is>
2016-05-12 22:16:37 +0000

    userTest

---
+++ MooshakV2/MooshakV2/MooshakV2.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooshakV2.Services;
+
+namespace MooshakV2.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void TestGetAllUsers()
+        {
+            //Arrange:
+            const string user = "Admin";
+            var service = new UserService();
+
+            //Act:
+            var result = service.getAllUsers(user);
+
+            //Assert:
+            Assert.IsNotNull(user);
+
+        }
+    }
+}
