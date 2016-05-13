a03d1569ae8bb67f141ed1ce01249e03948b6911
Sigurður Unnar <sigurduruh15@ru.is>
2016-05-13 00:43:44 +0000

    mockdatabase og meððví

--- MooshakV2/MooshakV2/MooshakV2.Tests/Services/UserServiceTest.cs
+++ MooshakV2/MooshakV2/MooshakV2.Tests/Services/UserServiceTest.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MooshakV2.Services;
+using MooshakV2.Tests;
+using MooshakV2.Models.entities;
 
 namespace MooshakV2.Tests.Services
 {
     [TestClass]
     public class UserServiceTest
     {
+        private UserService _service;
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockDb = new MockDataContext
+            _service = new UserService();
+
+        }
         [TestMethod]
         public void TestGetAllUsers()
         {
@@ -15,7 +25,7 @@ namespace MooshakV2.Tests.Services
             var service = new UserService();
 
             //Act:
-            var result = service.getAllUsers(user);
+            var result = _service.getAllUsers(user);
 
             //Assert:
             Assert.IsNotNull(user);
